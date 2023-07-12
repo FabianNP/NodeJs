14ac1a59cc7e26e2edf398a262c353445dbb73a7
Fabian <burner.afire@gmail.com>
2023-07-11 21:35:28 -0600

    Added the managment with express

--- express/server.js
+++ express/server.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require("path")
 const PORT = process.env.PORT || 3500
 
+//Route handlers 
 app.get("^/$|/index(.html)?", (req, res) => {
   // res.sendFile("./views/index.html", { root: __dirname })
   res.sendFile(path.join(__dirname, "views", "index.html"))
@@ -16,7 +17,6 @@ app.get("/old-page(.html)?", (req, res) => {
   res.redirect(301, "/new-page.html") //302 by default
 })
 
-//Route handlers 
 app.get("/hello(.html)?", (req, res, next) => {
   console.log("attempted to load hello.html")
   next()
