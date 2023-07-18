087c2093b5876bea0892abb35822284f9a063832
Fabian <burner.afire@gmail.com>
2023-07-18 13:37:09 -0600

    CRUDMongo

--- 12.-MongoDB/controllers/logoutController.js
+++ 12.-MongoDB/controllers/logoutController.js
@@ -1,36 +1,25 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function(data) {
-    this.users = data
-  }
-}
-
-const fsPromises = require("fs").promises
-const path = require("path")
+const User = require("../model/User")
 
 const handleLogout = async (req, res) => {
   //On client, also delete the accessToken
+
   const cookies = req.cookies
   if(!cookies?.jwt) return res.status(204)
   const refreshToken = cookies.jwt
 
   //Is refreshToken in db?
-  const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken )
+  const foundUser = await User.findOne({ refreshToken }).exec()
   if(!foundUser){
     res.clearCookie("jwt", { httpOnly: true })
     return res.sendStatus(403)
   } 
   
  // Delete refreshToken in db
-  const othersUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken )
-  const currentUser = {...foundUser, refreshToken: "" }
-  usersDB.setUsers([...othersUsers, currentUser])
-  await fsPromises.writeFile(
-    path.join(__dirname, "../model", "users.json"),
-    JSON.stringify(usersDB.users)
-  )
+  foundUser.refreshToken = ""
+  const result = await foundUser.save()
+  console.log(result)
 
-  res.clearCookie("jwt", { httpOnly: true, secure: true }) // secure: true - only serves on hhtps
+  res.clearCookie("jwt", { httpOnly: true }) 
   res.sendStatus(204)
 }
 
